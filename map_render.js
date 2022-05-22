f9729e6bd26f8c16ce0841008c2259321f5713c8
Johnny McKenzie <iamd2204@gmail.com>
2022-05-22 12:31:28 +0100

    updated icon control logic

--- map_render.js
+++ map_render.js
@@ -21,6 +21,10 @@ var barrierIcon = L.icon({
   popupAnchor: [16, 16]
 });
 
+function swapsies(n) {
+  return [n[1], n[0]];
+}
+
 var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 18,
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -29,5 +33,21 @@ var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
   tileSize: 512,
   zoomOffset: -1
 }).addTo(map);
-var geojsonLayer = new L.GeoJSON.AJAX("mapScripts/bicycleAmenities.geojson");       
-geojsonLayer.addTo(map);
+var geojsonLayer = new L.GeoJSON.AJAX("mapScripts/bicycleAmenities.geojson", {
+  onEachFeature: function (feature, layer) {
+    if (feature.properties && feature.properties.amenity) {
+      switch(feature.properties.amenity) {
+        case "bicycle_parking":
+          L.marker(swapsies(feature.geometry.coordinates), {icon: rackIcon}).addTo(map);
+          break;
+        case "cycle_barrier":
+          L.marker(swapsies(feature.geometry.coordinates), {icon: barrierIcon}).addTo(map);
+          break;
+        case "bicycle_repair_station":
+          L.marker(swapsies(feature.geometry.coordinates), {icon: repairIcon}).addTo(map);
+        default:
+      }
+    }
+  }
+});
+//geojsonLayer.addTo(map);
